33eccafce3e4189f069c51287cbfff7a8080c51a
Luciano <lcarhuaricra.hdc@gmail.com>
2018-06-13 14:24:56 -0500

    Agregar avances en la aplicación móvil de RSAC

---
+++ Desarrollo/RSAC/Codigo Fuente/frontend/android/Flavr/app/src/main/java/com/iss/flavr/presentation/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.iss.flavr.presentation.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iss.flavr.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
